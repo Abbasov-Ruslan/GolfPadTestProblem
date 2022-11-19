37ce2a20b9919645f65c4da812efe06b6d1f3d8c
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-11-19 15:07:13 +0400

    add numbers change func to viewModel

--- GolfPadTestProblem/Code/ViewModel/CourseHandicapViewModel.swift
+++ GolfPadTestProblem/Code/ViewModel/CourseHandicapViewModel.swift
@@ -10,11 +10,44 @@ import Combine
 struct CourseHandicapViewModel {
 
     var playerData = PlayerData()
-    let mySubject = PassthroughSubject<Float, Never>()
+    let handicapIndexSubject = CurrentValueSubject<Float, Never>(0)
+    let slopeRatinIndexSubject = CurrentValueSubject<Float, Never>(0)
+    let courseRatingSubject = CurrentValueSubject<Float, Never>(0)
+    let parSubject = CurrentValueSubject<Float, Never>(0)
+
+    private var cancellables = Set<AnyCancellable>()
 
     func countCourseHandiCap(playerData: PlayerData) -> Float{
         return playerData.handicapIndex * (playerData.slopeRating / 113) + (playerData.courseRate - playerData.par)
     }
+
+    public func handicapIndexChange(handicapIndex: String) {
+        let handicapIndexNumber = convertStringToFloat(string: handicapIndex)
+        handicapIndexSubject.send(handicapIndexNumber)
+    }
+
+    public func slopeRatingChange(slopeRating: String) {
+        let slopeRatingNumber = convertStringToFloat(string: slopeRating)
+        slopeRatinIndexSubject.send(slopeRatingNumber)
+    }
+
+    public func courseRatingChange(courseRating: String) {
+        let courseRatingNumber = convertStringToFloat(string: courseRating)
+        courseRatingSubject.send(courseRatingNumber)
+    }
+
+    public func parChange(par: String) {
+        let parNumber = convertStringToFloat(string: par)
+        courseRatingSubject.send(parNumber)
+    }
+
+    private func convertStringToFloat(string: String) -> Float {
+        guard let number = Float(string) else {
+            return 0
+        }
+        return number
+    }
+
 }
 
 struct PlayerData {
