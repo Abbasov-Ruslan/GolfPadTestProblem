346e4c882c0ec45b12971780cf3ed2651e5df8df
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-11-19 15:05:47 +0400

    add publishers for textfields to view controller

--- GolfPadTestProblem/UI/CourseHandicap/CourseHandicapCalcViewController.swift
+++ GolfPadTestProblem/UI/CourseHandicap/CourseHandicapCalcViewController.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+import Combine
 
 class CourseHandicapCalcViewController: UIViewController {
 
+    private var cancellables = Set<AnyCancellable>()
+    private var viewModel = CourseHandicapViewModel()
+
+    @IBOutlet private weak var handicapIndexTextField: UITextField!
+    @IBOutlet private weak var slopeRatingTextField: UITextField!
+    @IBOutlet private weak var courseRatingTextField: UITextField!
+    @IBOutlet private weak var parTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFieldsPublisher()
+
+    }
+
+    private func setupTextFieldsPublisher() {
+        addTextFieldPublisher(textField: handicapIndexTextField)
+        addTextFieldPublisher(textField: slopeRatingTextField)
+        addTextFieldPublisher(textField: courseRatingTextField)
+        addTextFieldPublisher(textField: parTextField)
+    }
+
+    private func addTextFieldPublisher(textField: UITextField) {
+        textField
+            .publisher(for: .editingChanged)
+            .debounce(for: .milliseconds(1000), scheduler: DispatchQueue.main)
+            .sink { [weak self] in
+                self?.viewModel.handicapIndexChange(handicapIndex: textField.text ?? "")
+            }.store(in: &cancellables)
+
     }
 
 }
